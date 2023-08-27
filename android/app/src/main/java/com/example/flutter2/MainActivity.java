49be4488f10c483197062bf8780a9bf36471787b
BingHao <hohao129@gmail.com>
2023-08-28 03:46:40 +0800

    Added some files to try making background notification visible after closing

--- android/app/src/main/java/com/example/flutter2/MainActivity.java
+++ android/app/src/main/java/com/example/flutter2/MainActivity.java
@@ -1,6 +1,28 @@
 package com.example.flutter2;
-
+import android.os.Bundle;
 import io.flutter.embedding.android.FlutterActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 
 public class MainActivity extends FlutterActivity {
+    private static final String CHANNEL_ID = "firebase_test_1"; // Replace with your channel ID
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Create notification channel
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
 }
